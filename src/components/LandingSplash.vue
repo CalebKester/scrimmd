06bb7383ff8ad97d31781cd7f963f82b5c4c7355
Caleb Kester <calebkester@gmail.com>
2018-01-09 19:06:07 -0600

    splash screen

---
+++ src/components/LandingSplash.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-screen h-screen relative flex items-center justify-center">
+    <img class="pin fixed w-screen h-screen object-fit-cover" src="@/assets/splash.jpg" alt="" />
+    <img class="relative w-64" src="@/assets/logo.svg" alt="scrimmd" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Hello from "@/components/Hello";
+import LandingSplash from "@/components/LandingSplash";
 
 Vue.use(Router);
 
@@ -9,6 +10,11 @@ export default new Router({
   routes: [
     {
       path: "/",
+      name: "LandingSplash",
+      component: LandingSplash
+    },
+    {
+      path: "/hello",
       name: "Hello",
       component: Hello
     }
